2677892db3f6d57bc65491612ed8faff932e4007
Howard Zhou <howardzhou@m800.com>
2019-12-25 14:18:10 +0800

    task: update 32

--- practices/32/go.mod
+++ practices/32/go.mod
@@ -1,3 +1,11 @@
 module github.com/lovemew67/go-misc/practices/32
 
 go 1.13
+
+require (
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/uber/jaeger-client-go v2.21.1+incompatible
+	github.com/uber/jaeger-lib v2.2.0+incompatible
+	go.uber.org/atomic v1.5.1 // indirect
+)
--- practices/32/main.go
+++ practices/32/main.go
@@ -1,7 +1,17 @@
 package main
 
 import (
+	"io"
 	"log"
+	"time"
+
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-lib/metrics"
 )
 
 func init() {
@@ -10,5 +20,54 @@ func init() {
 }
 
 func main() {
+	sConf := &jaegercfg.SamplerConfig{
+		Type:  jaeger.SamplerTypeRateLimiting,
+		Param: 1.0,
+	}
+	rConf := &jaegercfg.ReporterConfig{
+		QueueSize:           128,
+		BufferFlushInterval: 10 * time.Second,
+		LocalAgentHostPort:  "localhost:6831",
+		LogSpans:            true,
+	}
+	closer, errJaeger := InitJaeger("32", sConf, rConf)
+	if errJaeger != nil {
+		panic(errJaeger)
+	}
+	defer closer.Close()
 
+	var sp opentracing.Span
+	testCarrier := map[string]string{}
+	wireContext, err := opentracing.GlobalTracer().Extract(
+		opentracing.TextMap,
+		opentracing.TextMapCarrier(testCarrier))
+	if err != nil {
+		log.Printf("fuck err: %+v", err)
+		sp = opentracing.StartSpan("span a")
+	} else {
+		sp = opentracing.StartSpan(
+			"span a",
+			ext.RPCServerOption(wireContext))
+	}
+	sp.SetTag("yoyo", "haha")
+	defer sp.Finish()
+}
+
+func InitJaeger(componentName string, samplerConf *jaegercfg.SamplerConfig, reporterConf *jaegercfg.ReporterConfig) (io.Closer, error) {
+	cfg := jaegercfg.Configuration{
+		ServiceName: componentName,
+		Sampler:     samplerConf,
+		Reporter:    reporterConf,
+	}
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+	tracer, closer, err := cfg.NewTracer(
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
+	)
+	if err != nil {
+		return nil, err
+	}
+	opentracing.SetGlobalTracer(tracer)
+	return closer, nil
 }
